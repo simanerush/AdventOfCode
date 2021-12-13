ce62f31a7bfef77cf69396f9f32826510964b2e6
Serafima Nerush <2002ssn@gmail.com>
2021-12-13 13:14:55 -0800

    add day 12

--- AdventOfCode/Days/Day12.swift
+++ AdventOfCode/Days/Day12.swift
@@ -6,27 +6,67 @@
 import Foundation
 
 final class Day12: Day {
+    
+    func findPoints(dict: [String: [String]], alreadyVisited: [String], start: String, visitedTwice: Bool = false) -> Int {
+        var twice = false
+        if start == "start" && alreadyVisited != [] {
+            return 0
+        }
+        if start == "end" {
+            return 1
+        }
+        if start.lowercased() == start && alreadyVisited.contains(start) && visitedTwice {
+            return 0
+            
+        } else if start.lowercased() == start && alreadyVisited.contains(start) {
+            twice = true
+        }
+        var result = 0
+        for value in dict[start]! {
+            result += findPoints(dict: dict, alreadyVisited: alreadyVisited + [start], start: value, visitedTwice: visitedTwice || twice)
+        }
+        return result
+    }
+    
     func part1(_ input: String) -> CustomStringConvertible {
-        var dict: [String: [String]] = [:]
+        var graph: [String: [String]] = [:]
         input
             .lines
             .map { line in line.split(separator: "-") }
             .forEach { pair in
-                if dict[String(pair[0])] != nil{
-                    dict[String(pair[0])]!.append(String(pair[1]))
+                if graph[String(pair[0])] != nil{
+                    graph[String(pair[0])]!.append(String(pair[1]))
                 } else {
-                    dict[String(pair[0])] = [String(pair[1])]
+                    graph[String(pair[0])] = [String(pair[1])]
                 }
-                if dict[String(pair[1])] != nil{
-                    dict[String(pair[1])]!.append(String(pair[0]))
+                if graph[String(pair[1])] != nil{
+                    graph[String(pair[1])]!.append(String(pair[0]))
                 } else {
-                    dict[String(pair[1])] = [String(pair[0])]
+                    graph[String(pair[1])] = [String(pair[0])]
                 }
             }
-        return dict
+        
+        return findPoints(dict: graph, alreadyVisited: [], start: "start", visitedTwice: true)
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
-        return 0
+        var graph: [String: [String]] = [:]
+        input
+            .lines
+            .map { line in line.split(separator: "-") }
+            .forEach { pair in
+                if graph[String(pair[0])] != nil{
+                    graph[String(pair[0])]!.append(String(pair[1]))
+                } else {
+                    graph[String(pair[0])] = [String(pair[1])]
+                }
+                if graph[String(pair[1])] != nil{
+                    graph[String(pair[1])]!.append(String(pair[0]))
+                } else {
+                    graph[String(pair[1])] = [String(pair[0])]
+                }
+            }
+        
+        return findPoints(dict: graph, alreadyVisited: [], start: "start")
     }
 }
--- AdventOfCode/Days/Day13.swift
+++ AdventOfCode/Days/Day13.swift
@@ -6,8 +6,51 @@
 import Foundation
 
 final class Day13: Day {
+    
+    struct Coordinate: Hashable {
+        let x: Int
+        let y: Int
+    }
+    
+    func fold(over val: Int, set: Set<Coordinate>) -> Set<Coordinate> {
+        var folded: Set<Coordinate> = []
+        for coord in set {
+            if coord.x > val {
+                assert(2*val - coord.x >= 0)
+                folded.insert(Coordinate(x: 2*val - coord.x, y: coord.y))
+            } else if coord.x < val {
+                folded.insert(Coordinate(x: coord.x, y: coord.y))
+            }
+        }
+        
+        return folded
+    }
+    
     func part1(_ input: String) -> CustomStringConvertible {
-        return 0
+        let parsedInput = input
+            .lines
+        
+        let point = parsedInput
+            .firstIndex { line in
+                line.starts(with: "f")
+            }
+        
+        let coordinates = parsedInput[0..<point!]
+            .map { coord -> (Int, Int) in
+                let string = coord.split(separator: ",")
+                return (Int(String(string[0]))!, Int(String(string[1]))!)
+            }
+        let commands = parsedInput[point!..<parsedInput.count]
+        
+        var hashtagSet: Set<Coordinate> = []
+        
+        for coordinate in coordinates {
+            hashtagSet.insert(Coordinate(x: coordinate.0, y: coordinate.1))
+        }
+        
+        let folded = fold(over: 655, set: hashtagSet)
+        
+        return folded
     }
 
     func part2(_ input: String) -> CustomStringConvertible {
